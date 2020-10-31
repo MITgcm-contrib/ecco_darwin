de422ce9b1721e1701036b2391e743d8d5db83d7
Dustin Carroll <61992971+dustincarrollocean@users.noreply.github.com>
2020-10-31 14:12:23 -0700

    v05 budget code and diagnostics update

---
+++ v05/llc270/code_darwin/DARWIN_DIAGS.h
@@ -0,0 +1,49 @@
+#ifdef ALLOW_DARWIN
+
+CBOP
+C    !ROUTINE: DARWIN_DIAGS.h
+C    !INTERFACE:
+C #include DARWIN_DIAGS.h
+
+C    !DESCRIPTION:
+C Contains indices into diagnostics array
+
+      integer iPP
+      integer iNfix
+      integer iDenit
+      integer iDenitN
+      integer iPPplank
+      integer iGRplank
+      integer iGrGn
+      integer iConsDIN
+      integer iConsNO3
+      integer iConsNO2
+      integer iConsNH4
+      integer iConsPO4
+      integer iConsSi
+      integer iConsFe
+      integer darwin_nDiag
+      PARAMETER(iPP=     1)
+      PARAMETER(iNfix=   2)
+      PARAMETER(iDenit=  3)
+      PARAMETER(iDenitN= 4)
+      PARAMETER(iConsPO4=5)
+      PARAMETER(iConsSi= 6)
+      PARAMETER(iConsFe= 7)
+      PARAMETER(iConsDIN=8)
+      PARAMETER(iPPplank=9)
+      PARAMETER(iConsNO3=10)
+      PARAMETER(iConsNO2=11)
+      PARAMETER(iConsNH4=12)
+#ifdef DARWIN_DIAG_PERTYPE
+      PARAMETER(iGRplank=iPPplank+nplank)
+      PARAMETER(iGrGn=iGRplank+nplank)
+      PARAMETER(darwin_nDiag=iGrGn+nplank-1)
+#else
+      PARAMETER(iGRplank=iConsNH4)
+      PARAMETER(iGrGn=iGRplank)
+      PARAMETER(darwin_nDiag=iGrGn-1)
+#endif
+
+CEOP
+#endif /* ALLOW_DARWIN */
