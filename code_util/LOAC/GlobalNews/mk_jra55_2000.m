08264fda62eb0a4fb985db39ed8c50824d5fd1fb
Dimitris Menemenlis <menemenlis@jpl.nasa.gov>
2022-03-03 08:00:27 -0800

    bug fixes to match /nobackup/dcarrol2/LOAC/bin/jra55_do/v1.4.0 values

--- code_util/LOAC/GlobalNews/mk_jra55_2000.m
+++ code_util/LOAC/GlobalNews/mk_jra55_2000.m
@@ -1,32 +1,36 @@
 clear, close all
-clear, close all
 
-% pathnames below are on pleiades
-pn1='~dmenemen/forcing/jra55_do/';
-pn2='/nobackup/dcarrol2/LOAC/bin/jra55_do/v1.4.0/';
+% pathname below is on pleiades
+pnm='/nobackup/dcarrol2/LOAC/bin/jra55_do/v1.4.0/';
 
 % Compute jra55_do time-mean year-2000 runoff
 yr=2000;
 loy=365;
 if mod(yr,4)==0, loy=366; end
 
+% define grid cell longitude, latitude, and area
 lon=0.125:0.25:360;
 lat=-89.875:0.25:90;
 nx=length(lon);
 ny=length(lat);
 [jlat jlon]=meshgrid(lat,lon);
-
-fin=[pn1 'cellarea.bin'];
+fin=[pnm 'cellarea.bin'];
 cellarea=readbin(fin,[nx ny]);  % area of grid cell in m^2
 
-fin=[pn1 'jra55_do_runoff_' int2str(yr)];
+% read and cumulate runoff
+fin=[pnm 'jra55_do_runoff_' int2str(yr)];
 jra=readbin(fin,[nx ny loy]);              % load whole year, units are m/s
 jra=sum(jra,3).*cellarea*24*60*60/1e9;     % cumulate & convert to km^3/yr
 
+% convert to sparse vectors
 jlon=jlon(:);
 jlat=jlat(:);
 jra=jra(:);
-ix=find(jra==0); jlon(ix)=0; jlat(ix)=0;
-jra=sparse(jra); jlon=sparse(jlon); jlat=sparse(jlat);
+ix=find(jra==0);
+jlon(ix)=0;
+jlat(ix)=0;
+jra=sparse(jra);
+jlon=sparse(jlon);
+jlat=sparse(jlat);
 
 save jra55_2000 j*
