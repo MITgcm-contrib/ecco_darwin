d65cc5f3b98e1be9438eb6aa6fa7e24265ec1f72
rsavelli1617 <99680554+rsavelli1617@users.noreply.github.com>
2022-03-11 11:39:52 -0800

    Update SnapGlobalNEWS.m

--- code_util/LOAC/GlobalNews/SnapGlobalNEWS.m
+++ code_util/LOAC/GlobalNews/SnapGlobalNEWS.m
@@ -40,6 +40,13 @@ jraWeights=jra./gQact(gQact2jra);
 % Projecct GlobalNEWS2 nutrients to JRA55 locations
 pin='/nobackup/dcarrol2/LOAC/bin/jra55_do/v1.4.0/';
 pout='~dmenemen/forcing/jra55_do/GlobalNEWS/GlobalNEWS2_on_jra55v1.4.0/';
+
+% conversion factors of gram to mol
+gP_to_molP = 0.03228539149637;
+gN_to_molN = 0.071394404106606;
+gC_to_molC = 0.083259093974539;
+gSi_to_molSi = 0.03560556158872;
+
 for yr=1991:2021
     fin=[pin 'jra55_do_runoff_' int2str(yr)];
     loy=365;
@@ -50,7 +57,23 @@ for yr=1991:2021
             fout=[pout f{1} '_' int2str(yr)];
             eval(['fld=g' f{1} ';'])
             FLD=0*LAT;
-            FLD(IX)=fld(gQact2jra).*jraWeights;
+            %FLD(IX)=fld(gQact2jra).*jraWeights;
+            FLD(IX)=fld(gQact2jra)./gQact(gQact2jra).*jraWeights./1e9.*Jravol.*1e6
+            % result in g m-2 s-1
+            % 1e9 conversion from km-3 to m-3
+            % 1e6 conversion from Mg to g
+            % Following conditions are to convert g to mmol
+            	if f{1 end} == 'N'
+            		FLD(IX) = FLD(IX).*gN_to_molN.*1e3;
+            	elseif f{1 end} == 'P'
+            		FLD(IX) = FLD(IX).*gP_to_molP.*1e3;
+            	elseif f{1 end} == 'C'
+            		FLD(IX) = FLD(IX).*gC_to_molC.*1e3;
+            	elseif f{1 2end} == 'Si'
+            		FLD(IX) = FLD(IX).*gSi_to_molSi.*1e3;            		
+            	else %no C, N, P or Si
+            		%do nothing: TSS remain in g m-2 s-1
+            	end	
             writebin(fout,FLD,1,'real*4',dy-1);
         end
     end
