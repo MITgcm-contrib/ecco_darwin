ef053005df501a2bb56c700326b505b615037941
rsavelli1617 <99680554+rsavelli1617@users.noreply.github.com>
2022-11-29 11:02:36 -0800

    Implementation of the RADI model in v05 llc270_sed

--- v05/llc270_sediment/code_darwin/DIAGNOSTICS_SIZE.h
+++ v05/llc270_sediment/code_darwin/DIAGNOSTICS_SIZE.h
@@ -17,7 +17,7 @@ C  and "diagSt_size" (statistics-diags) since values here are deliberately small
       INTEGER    nRegions, sizRegMsk, nStats
       INTEGER    diagSt_size
       PARAMETER( ndiagMax = 1500 )
-      PARAMETER( numlists = 94, numperlist = 94, numLevels=Nr )
+      PARAMETER( numlists = 116, numperlist = 116, numLevels=Nr )
       PARAMETER( numDiags = 25+300*Nr )
       PARAMETER( nRegions = 0 , sizRegMsk = 1 , nStats = 4 )
       PARAMETER( diagSt_size = 10*Nr )
