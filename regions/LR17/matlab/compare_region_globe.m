c729204381135c873e4020d46306e98018e6dd4d
Dimitris Menemenlis <dmenemenlis@gmail.com>
2023-07-28 17:43:36 -0700

    updates to compare_region_globe.m for LR17 region

--- regions/LR17/matlab/compare_region_globe.m
+++ regions/LR17/matlab/compare_region_globe.m
@@ -1,7 +1,8 @@
 % Compare regional results to the global simulation
-nx=80; ny=100;
-p1='/nobackup/dmenemen/CCS_kelp/';
-p2='/nobackup/dmenemen/ecco_darwin/darwin3/run/diags/';
+nx=10; ny=22; nz=46;
+s1='_10x22.'; s2='_10x22x46.';
+p1='~/Links/Box/Public/LR17/';
+p2='~/mitgcm/darwin3/run/diags/';
 
 % Compare some monthly-mean values
 ts=5472;
@@ -20,6 +21,44 @@ for fld={'ALK','c1','c2','c3','c4','c5','c6','c7','Chl1','Chl2','Chl3', ...
     clf, subplot(121), mypcolor(fl1'), colorbar('h')
     title([fld{1} ' ' ts2dte(ts,1200,1992,1,16,30)])
     subplot(122), mypcolor(fl2(:,:,1)'-fl1'), colorbar('h')
+    in=find(~isnan(fl1));
+    title(['regional-global: ' num2str((rms(fl2(in)-fl1(in)))/rms(fl1(in)))])
+    pause
+end
+
+% Compare some monthly-mean values
+fld={'SALT'};
+for ts=[1152 3240 5472 7632 9864 12024 14256 16488 18648 20880 23040]
+    fn=[p1 fld{1} '/' fld{1} s2 ts2dte(ts,1200,1992,1,16,30)];
+    fl1=readbin(fn,[nx ny]);
+    if strcmp(fld{1},'SALT')
+        fl2=35+rdmds([p2 'monthly/SALTanom'],ts);
+    else
+        fl2=rdmds([p2 'monthly/' fld{1}],ts);
+    end
+    in=find(fl1==0); fl1(in)=nan; fl2(in)=nan;
+    clf, colormap(jet), subplot(121), mypcolor(fl1'), colorbar('h')
+    title([fld{1} ' ' ts2dte(ts,1200,1992,1,16,30)])
+    subplot(122), mypcolor(fl2(:,:,1)'-fl1'); caxis([-1 1]*.3); colorbar('h')
+    in=find(~isnan(fl1));
+    title(['regional-global: ' num2str((rms(fl2(in)-fl1(in)))/rms(fl1(in)))])
+    pause
+end
+
+% Compare some monthly-mean values
+fld={'THETA'};
+for ts=[1152 3240 5472 7632 9864 12024 14256 16488 18648 20880 23040]
+    fn=[p1 fld{1} '/' fld{1} s2 ts2dte(ts,1200,1992,1,16,30)];
+    fl1=readbin(fn,[nx ny]);
+    if strcmp(fld{1},'SALT')
+        fl2=35+rdmds([p2 'monthly/SALTanom'],ts);
+    else
+        fl2=rdmds([p2 'monthly/' fld{1}],ts);
+    end
+    in=find(fl1==0); fl1(in)=nan; fl2(in)=nan;
+    clf, colormap('default'), subplot(121), mypcolor(fl1'),caxis([10 14]), colorbar('h')
+    title([fld{1} ' ' ts2dte(ts,1200,1992,1,16,30)])
+    subplot(122), mypcolor(fl2(:,:,1)'-fl1'); caxis([-1 1]*3); colorbar('h')
     in=find(~isnan(fl1));
     title(['regional-global: ' num2str((rms(fl2(in)-fl1(in)))/rms(fl1(in)))])
     pause
