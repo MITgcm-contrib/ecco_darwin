a07ebbc5ee8995a944de4e6a21c043bd6a56fd82
Dimitris Menemenlis <dmenemenlis@gmail.com>
2024-10-30 07:45:31 -0700

    adding sbatch job for zorbas

---
+++ regions/Med/v05/input/run_on_zorbas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=MEDnp8           # Job name
+#SBATCH --output=MEDnp8.out         # Standard output and error log
+#SBATCH --error=MEDnp8.err          # Error log
+#SBATCH --partition=batch           # Error log
+#SBATCH --nodes=2                   # Number of nodes
+#SBATCH --ntasks-per-node=4         # Number of MPI tasks per node (adjust as needed)
+#SBATCH --partition=compute         # Partition name
+
+# Run the MPI job
+cd ~/darwin3/run
+mpirun -np 8 ./mitgcmuv
