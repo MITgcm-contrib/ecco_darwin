dc05e62d670a076cb79a6fbbcef0a20e4709de2e
xrios01 <112573726+xrios01@users.noreply.github.com>
2025-07-11 16:49:51 -0700

    7/11 Update pCO2 main.py

--- code_util/LOAC/C_GEM/Elwha_River/main.py
+++ code_util/LOAC/C_GEM/Elwha_River/main.py
@@ -9,6 +9,7 @@ from transport_module import transport
 from biogeo_module import biogeo
 from sed_module import sed
 from config import MAXT, DELTI, WARMUP
+from pre_model_stratup_check import check_output_writability
 
 def main():
     """Main model routine."""
@@ -37,4 +38,36 @@ def main():
             sed(t)
 
 if __name__ == "__main__":
+
+# list of all output files to write to
+    output_files = [
+    "depth.dat",     # water depth data
+    "width.dat",     # estuarine width data
+    "S.dat",         # salinity data
+    "SPM.dat",       # suspended particulate matter concentrations
+    "DIA.dat",       # diatoms concentrations
+    "NH4.dat",       # ammonium concentrations
+    "NO3.dat",       # nitrate concentrations
+    "O2.dat",        # oxygen concentrations
+    "PO4.dat",       # phosphate concentrations
+    "DSi.dat",       # dissolved silica concentrations
+    "TOC.dat",       # total organic carbon concentrations
+    "NPP.dat",       # Net Primary Production rates
+    "aer_deg.dat",   # aerobic degradation rates
+    "denit.dat",     # denitrification rates
+    "nit.dat",       # nitrification rates
+    "O2_ex.dat",     # air-water O₂ exchange rates
+    "NEM.dat",       # Net Ecosystem Metabolism rates
+
+    # FCO2 version outputs:
+    "DIC.dat",       # dissolved inorganic carbon concentration
+    "ALK.dat",       # alkalinity
+    "pH.dat",        # pH
+    "FCO2.dat"       # air-water CO₂ exchange rates
+    ]
+
+    if not check_output_writability(output_files):
+        exit("Aborting: one or more files are locked or not writable.")
+
+
     main()
