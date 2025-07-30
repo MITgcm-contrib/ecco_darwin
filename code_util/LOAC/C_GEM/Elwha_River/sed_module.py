bb28a2961809968114655ff9d05cf2210eb0e832
xrios01 <112573726+xrios01@users.noreply.github.com>
2025-07-30 14:27:36 -0700

    7/30 sediment Update sed_module.py

--- code_util/LOAC/C_GEM/Elwha_River/sed_module.py
+++ code_util/LOAC/C_GEM/Elwha_River/sed_module.py
@@ -4,9 +4,11 @@ Sediment module (translated from sed.c)
 
 from config import (
     M, rho_w, G, Chezy_lb, Chezy_ub, Mero_lb, Mero_ub,
-    tau_ero_lb, tau_ero_ub, tau_dep_lb, tau_dep_ub, distance, ws, DELTI
+    tau_ero_lb, tau_ero_ub, tau_dep_lb, tau_dep_ub, distance, ws, DELTI, TS, WARMUP
 )
 from variables import U, DEPTH, v, tau_b, Mero, tau_ero, tau_dep, erosion, deposition, Chezy, include_constantDEPTH
+from forcings_module import get_sediment
+from file_module import Rates
 
 def sed(t):
     """Calculate sediment erosion and deposition rates."""
@@ -39,4 +41,20 @@ def sed(t):
             tau_dep[i] = tau_dep_ub if i >= distance else tau_dep_lb
 
         # Update SPM concentration [g/l]
-        v['SPM']['c'][i] = v['SPM']['c'][i] + (1.0 / DEPTH[i]) * (erosion[i] - deposition[i]) * DELTI
+        #v['SPM']['c'][i] = v['SPM']['c'][i] + (1.0 / DEPTH[i]) * (erosion[i] - deposition[i]) * DELTI
+        #v['SPM']['c'][i] = get_sediment(t) + (1.0 / DEPTH[i]) * (erosion[i] - deposition[i]) * DELTI
+        if t <= WARMUP:
+            # pure physical model during warmup - let system equilibrate
+            v['SPM']['c'][i] = v['SPM']['c'][i] + (1.0 / DEPTH[i]) * (erosion[i] - deposition[i]) * DELTI
+        else:
+            # hybrid model after warmup - time series + physical processes
+            v['SPM']['c'][i] = get_sediment(t) + (1.0 / DEPTH[i]) * (erosion[i] - deposition[i]) * DELTI
+
+    # Write erosion/deposition process rates [mg m^-2 s^-1]
+    if (float(t) / float(TS * DELTI)) % 1 == 0:
+        Rates(erosion, "erosion.dat", t)
+        Rates(deposition, "deposition.dat", t)
+
+
+
+
